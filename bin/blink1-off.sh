ea8f09de23b245df5f25bdb685c4dcd06902c830
deors <jorge.hidalgo@gmail.com>
2020-02-10 21:06:23 +0100

    update blink1-tool path

--- bin/blink1-off.sh
+++ bin/blink1-off.sh
@@ -1,1 +1,1 @@
-sudo ~/code/projects/blink1/commandline/blink1-tool --off
+sudo ~/code/projects/blink1-tool/blink1-tool --off
--- bin/blink1-tool.sh
+++ bin/blink1-tool.sh
@@ -1,1 +1,1 @@
-sudo ~/code/projects/blink1/commandline/blink1-tool $1 $2 $3 $4 $5 $6 $7 $8 $9
+sudo ~/code/projects/blink1-tool/blink1-tool $1 $2 $3 $4 $5 $6 $7 $8 $9
